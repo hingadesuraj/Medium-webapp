1117c414ece2693c338b4dd2291acb11dba83d2c
Suraj <hingadesuraj@gmail.com>
2024-03-29 18:36:07 +0530

    token generate

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,46 +1,54 @@
 const User = require("../models/user");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSignup = async (req, res, next) => {
-    try {
-        const { firstName, lastName, email, password } = req.body;
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-        // Check if the user already exists
-        const userExist = await User.findOne({ email });
+    // Check if the user already exists
+    const userExist = await User.findOne({ email });
 
-        if (userExist) {
-            return res.status(400).json({
-                success: false,
-                message: "User already exists. Please use another email.",
-            });
-        }
-
-        const saltRounds = 10;
-        const hashPass = await bcrypt.hash(password,saltRounds);
-        // console.log(hashPass)
-
-        // Create the user
-        await User.create({
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password,
-        });
-
-        return res.status(200).json({
-            success: true,
-            message: "User registration successful.",
-        });
-    } catch (error) {
-        // Handle any errors
-        console.error("Error during user signup:", error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error.",
-        });
+    if (userExist) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists. Please use another email.",
+      });
     }
+
+    const saltRounds = 10;
+    const hashPass = await bcrypt.hash(password, saltRounds);
+    // console.log(hashPass)
+
+    // Create the user
+    const user = await User.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashPass,
+    });
+
+    // generate token
+    const Id = user._id;
+    // console.log(userData._id)
+    const token = jwt.sign({ Id }, "Suraj");
+    // console.log(token)
+
+    return res.status(200).json({
+      success: true,
+      token:token,
+      message: "User registration successful.",
+    });
+  } catch (error) {
+    // Handle any errors
+    console.error("Error during user signup:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+    });
+  }
 };
 
 module.exports = {
-    userSignup,
+  userSignup,
 };
